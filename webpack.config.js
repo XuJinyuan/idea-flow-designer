0aec9381793221dd020eb2792f207269c2f38a91
Jason Xu(许金源) <xujy@inspur.com>
2020-04-18 17:03:17 +0800

    修复依赖的版本

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js'
+        filename: 'app.js'
     },
     module: {
         rules: [
